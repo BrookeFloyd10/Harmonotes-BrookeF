5d8edee3e9cd31ff952509aede9f8461ce345033
Brooke F <brooke.floyd10@gmail.com>
2026-07-06 16:53:52 -0500

    created shell for all pages and imported to main to guarentee they all render correctly

--- harmonotes/src/App.jsx
+++ harmonotes/src/App.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { useState } from 'react';
+import './App.css';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Library from './pages/Library';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './pages/About';
 
 function App() {
   return (
     <div id="body-container"> 
       <Header />
+      <main>
+        
+        <Home />
+        <Dashboard />
+        <About />
+        <Library />
+
+      </main>
 
       <Footer />
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ harmonotes/src/pages/Library.jsx
@@ -0,0 +1,11 @@
+
+
+function Library() {
+    return(
+        <div className="pages">
+        <h1>Your Library</h1>
+        </div>
+    )
+};
+
+export default Library;
