b57740fedd759609d613763cc23a094399aca278
Brooke F <brooke.floyd10@gmail.com>
2026-07-07 08:48:18 -0500

    set up simple navigation, placed links in header/ tested to verify they work.

--- harmonotes/src/components/Header.jsx
+++ harmonotes/src/components/Header.jsx
@@ -1,8 +1,18 @@
+import {Link} from 'react-router'
+
 const Header = () => {
     return (
         <header>
             <div id="header">
                 <em>Harmonotes</em>
+
+                <nav>
+                  <Link to="/">Home</Link>
+                  <Link to="/About">About</Link>
+                  <Link to="/Dashboard">Dashboard</Link>
+                  <Link to="/Library">Library</Link>
+                </nav>
+
             </div>
         </header>
     );
