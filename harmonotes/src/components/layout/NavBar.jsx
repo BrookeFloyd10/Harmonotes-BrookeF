a4eead6496b709c8f3908014399b9fdd38f1862e
Brooke F <brooke.floyd10@gmail.com>
2026-07-10 23:19:59 -0500

    mported  last icon and conected the links to the images

--- harmonotes/src/components/layout/NavBar.jsx
+++ harmonotes/src/components/layout/NavBar.jsx
@@ -1,13 +1,21 @@
-import {Link} from 'react-router'
+import {Link} from 'react-router';
+import eighthNote from '../../assets/images/eighthNote.svg';
+import sixteenthNote from '../../assets/images/sixteenthNote.svg';
+import quarterNote from '../../assets/images/quarterNote.svg';
+import halfNote from '../../assets/images/halfNote.svg';
 
-    
+const NavBar = () =>{
+    return(
     <nav>
         <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/dashboard">Dashboard</Link></li>
-            <li><Link to="/library">Library</Link></li>
+            <li><Link to="/"><img src={sixteenthNote} alt= "clickable 16th note icon" />Home</Link></li>
+            <li><Link to="/about"><img src={halfNote} alt= "clickable half note icon" />About</Link></li>
+            <li><Link to="/dashboard"><img src={eighthNote} alt= "clickable 8th note icon" />Dashboard</Link></li>
+            <li><Link to="/library"><img src={quarterNote} alt= "clickable quarter note icon" />Library</Link></li>
         </ul>
     </nav>
+    )
+}
+
 
 export default NavBar;
