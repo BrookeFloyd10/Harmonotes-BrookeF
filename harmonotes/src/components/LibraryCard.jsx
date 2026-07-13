eebb81cc2972ba691454411cbfd49997710871fb
Brooke F <brooke.floyd10@gmail.com>
2026-07-12 22:35:50 -0500

    created library card to dynamically render library page. used conditionals to ensure proper information was rendering based on type

--- harmonotes/src/components/LibraryCard.jsx
+++ harmonotes/src/components/LibraryCard.jsx
@@ -1,5 +1,32 @@
 
-const LibraryCard = ({})
+const LibraryCard = ({ item }) => {
+
+    const { title, type, artist, difficulty, file, audio } = item
+    
+    return (
+        <article>
+            <h2>{title}</h2>
+            {artist && <p>By: {artist}</p>}
+            {file && (
+                <a href={file} target="_blank" rel="noopener noreferrer">
+                    {type === "song" ? "View Sheet Music" 
+                    : type === "reference" ? "View Reference" 
+                    : type === "exercise" ? "View Exercise"
+                    : " "} </a>
+                    )}
+            {difficulty && <p>Difficulty level: {difficulty}</p>}
+            {audio.length > 0 && (
+                <div>
+                    <p>Play along!</p>
+                    {audio.map((audioClip) => (
+                        <a key={audioClip.file} href={audioClip.file} target="_blank" rel="noopener noreferrer">
+                            {audioClip.label}</a>
+                   ))}
+                </div>
+            )}
+        </article>
+    );
+};
 
 
 export default LibraryCard;
