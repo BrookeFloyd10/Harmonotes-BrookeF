b1c03577ffe2d2dd48f5b6e1ff10f80a10591cc9
Brooke F <brooke.floyd10@gmail.com>
2026-07-12 22:34:25 -0500

    moved use state declaration up with others to fix error

--- harmonotes/src/pages/Library.jsx
+++ harmonotes/src/pages/Library.jsx
@@ -9,6 +9,7 @@ const Library = () => {
     const [libraryData, setLibraryData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [instrumentFilter, setInstrumentFilter] = useState("all");
 
     useEffect(() => {
         const fetchLibraryData = async () => {
@@ -34,14 +35,13 @@ const Library = () => {
         if (isLoading) return <Loading />;
         if (error) return <ErrorMessage message={error} />
 
-        const [instrumentFilter, setInstrumentFilter] = usestate("all");
 
         const filterByInstrument = libraryData.filter(item => 
             instrumentFilter === "all" || item.instrument.includes(instrumentFilter));
 
-        const songs = libraryData.filter(item => item.type === "song");
-        const exercises = libraryData.filter(item => item.type === "exercise");
-        const resources = libraryData.filter(item => item.type === "resource");
+        const songs = filterByInstrument.filter(item => item.type === "song");
+        const exercises = filterByInstrument.filter(item => item.type === "exercise");
+        const resources = filterByInstrument.filter(item => item.type === "reference");
 
         return(
             <>
@@ -50,7 +50,6 @@ const Library = () => {
                 <option value="all">All Instruments</option>
                 <option value="guitar">Guitar</option>
                 <option value="piano">Piano</option>
-
             </select>
 
             {songs.length > 0 && (
@@ -58,7 +57,7 @@ const Library = () => {
                     <h2>Songs:</h2>
                     <ul>
                         {songs.map((item) =>
-                            <li key={item.id}><LibraryCard file={item} />
+                            <li key={item.id}><LibraryCard item={item} />
                         </li>)}
                     </ul>
                 </div>
@@ -69,19 +68,19 @@ const Library = () => {
                     <h2>Exercises:</h2>
                     <ul>
                         {exercises.map((item) =>
-                            <li key={item.id}><LibraryCard file={item} />
+                            <li key={item.id}><LibraryCard item={item} />
                         </li>)}
                     </ul>
                 </div>
             )}
                 
 
-            {songs.length > 0 && (
+            {resources.length > 0 && (
                 <div className="resources">
                     <h2>Theory & Guides:</h2>
                     <ul>
-                        {songs.map((item) =>
-                            <li key={item.id}><LibraryCard file={item} />
+                        {resources.map((item) =>
+                            <li key={item.id}><LibraryCard item={item} />
                         </li>)}
                     </ul>
                 </div>
