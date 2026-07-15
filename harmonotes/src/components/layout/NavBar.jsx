12305b239e7199fa1e8e16f9f1c121d1bc321ba2
Brooke F <brooke.floyd10@gmail.com>
2026-07-15 16:29:10 -0500

    removed fixed heights and replaced px with calculated equilivant in rem to allow more responsiveness and adjustments for media, removed object rule on navigation

--- harmonotes/src/components/layout/NavBar.jsx
+++ harmonotes/src/components/layout/NavBar.jsx
@@ -13,7 +13,7 @@ const NavBar = () =>{
             <li><Link to="/"><img src={eighthNote} alt= "8th note icon link to home page" />Home</Link></li>
             <li><Link to="/about"><img src={halfNote} alt= "half note icon link to about page" />About</Link></li>
             <li><Link to="/dashboard"><img src={sixteenthNote} alt= "16th note icon link to dashboard" />Dashboard</Link></li>
-            <li><Link to="/library"><img src={quarterNote} alt= "quarter note icon link to" />Library</Link></li>
+            <li><Link to="/library"><img src={quarterNote} alt= "quarter note icon link to library" />Library</Link></li>
         </ul>
     </nav>
     )
