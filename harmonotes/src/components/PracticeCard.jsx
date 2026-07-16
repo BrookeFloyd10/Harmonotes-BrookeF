673086c7e66df9b454cbd71a78c890a3330bf483
Brooke F <brooke.floyd10@gmail.com>
2026-07-16 01:42:47 -0500

    rearranged practice card so check boxes and xp are closer together, restyled checkbox to be larger, moved reference link down to the bottom for a better flow

--- harmonotes/src/components/PracticeCard.jsx
+++ harmonotes/src/components/PracticeCard.jsx
@@ -3,18 +3,19 @@ const PracticeCard = ({ exercise, onToggleComplete }) => {
     
     const { title, description, duration, xp, videoLink, completed } = exercise;
     return (
+        
         <article className="practice-card">
-          <h2>{title}</h2>
-          <p>{description}</p>
-          <a href={videoLink} target="_blank" rel="noopener noreferrer">Reference Video!</a>
-          <p>{duration}</p>
-          <p>{xp} XP</p>
-          <label htmlFor={`completed-${exercise.id}`}>
-          <input id={`completed-${exercise.id}`} 
-                 type="checkbox" 
-                 checked={completed} 
-                 onChange={() => onToggleComplete(exercise.id)}/>
-                 </label>
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <p>{duration}</p>
+            <label htmlFor={`completed-${exercise.id}`}>
+            <input id={`completed-${exercise.id}`} 
+                type="checkbox" 
+                checked={completed} 
+                onChange={() => onToggleComplete(exercise.id)}/>
+                </label>
+            <p>{xp} XP</p>
+            <a href={videoLink} target="_blank" rel="noopener noreferrer">Reference Video!</a>
         </article>
     );
 };
